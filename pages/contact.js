d78569c3eb6838c4a591428f660460eb91ccf310
patrick-rush <patrick.a.rush@gmail.com>
2021-05-20 17:22:25 -0400

    build out contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,19 @@
+import Image from 'next/image'
+
 export default function Contact() {
-    return <h1>Get in Touch!</h1>
+    return (
+		<div>
+			<h1>Get in Touch!</h1>
+			<div>
+				<div>Phone: 804-731-3726</div>
+				<div>Email: mail@patrickrush.tech</div>
+				<Image 
+					src="/images/2021-twitter-logo-blue.png"
+					height={20}
+					width={25}
+					alt="Twitter"
+				/>
+			</div>
+		</div>
+	)
 }
